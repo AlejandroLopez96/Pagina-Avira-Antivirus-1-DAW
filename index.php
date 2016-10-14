238e2593bcae6eb408c4eb8822d1f022381b6caf
AlejandroLopez96 <alexlopezortiz9@gmail.com>
2016-10-14 19:33:31 +0200

    Página de Información Avira Antivirus 1ºDAW

---
+++ index.php
@@ -0,0 +1,78 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<meta charset="utf-8"/>
+	<title> AviraAntivirus </title>
+	<link rel="stylesheet" href="estilo.css">
+	<link rel="stylesheet" href="fonts.css">
+	<link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+</head>
+
+	<body>
+	<main>
+	<section>
+	
+	<header>
+	<br>
+		<p><a id="principio"></a></p>
+		<p id="contador">Visitas: <?php 
+		include("prueba.php"); 
+		?></p>
+		<h1><img class="a" src="avira.png" height="42" width="42"/>AVIRA ANTIVIRUS<img class="a" src="avira.png" height="42" width="42"/></h1>
+		<nav class="menu">
+		<ul>
+			<li><a class="especial" href="index.php">El programa</a></li>
+			<li><a href="versiones.html">Versiones</a></li>
+			<li><a href="inst-desinst.html">Instalación y desinstalación</a></li>
+			<li><a href="menus.html">Menús</a></li>
+			<li><a href="operativa.html">Operativa</a></li>
+			<li><a href="desc-pre.html">Descarga y precio</a></li>
+			<li><a href="progsim.html">Programas similares</a></li>
+		</ul>
+		</nav>
+	</header>
+	<aside>
+	<blockquote class="s">
+	<div class="social">
+		<ul>
+			<li><a href="https://www.facebook.com/avira" target="_blank" class="icon-facebook" title="Facebook"></a></li>
+			<li><a href="https://twitter.com/avira?lang=es" target="_blank"class="icon-twitter" title="Twitter"></a></li>
+			<li><a href="https://plus.google.com/+avira" target="_blank" class="icon-google-plus" title="Google Plus"></a></li>
+			<li><a href="#" class="icon-youtube" title="YouTube"></a></li>
+			<li><a href="mailto:asdsss@gmail.com" class="icon-mail2" title="Email"></a></li>
+		</ul>
+	</div>
+	</blockquote>
+	<blockquote class="p">
+	<div class="subir">
+		<ul>
+			<li><a href="#principio" class="icon-point-up" title="Subir arriba"></a></li>
+		</ul>
+	</div>
+	</blockquote>
+	</aside>
+	<div id="contenedor">
+    <article class="content">
+		<header>
+            <h2 style="padding-left: 15px;">¿Qué es y para qué sirve el programa?</h2>
+		</header>
+			<p>Avira Operations GmbH & Co. es una compañía de seguridad informática alemana. Sus aplicaciones antivirus están basadas en el motor AntiVir, lanzado en 1988, comúnmente llamado Luke Filewalker. Se fundó bajo el nombre 
+			de "H+BEDV Datentechnik GmbH". Una de sus aplicaciones antivirus, AntiVir PersonalEdition Classic, multiplataforma (mac, windows, linux) y es gratuita para uso personal. El motor de Avira ha sido licenciado al antivirus Ashampoo y Webroot
+			WebWasher. Dispone de un software antivirus gratuito para uso personal y su principal rival es Avast!, aunque en las últimas pruebas éste ha sido inferior en varios aspectos frente a Avira. En aspectos como: Consumo de recursos, heurística, 
+			y una alta capacidad de detección de rootkits y troyanos.</p>
+			<p>Para más información, visite <a class="a" href="https://www.avira.com/es/press-details" target="_blank">www.avira.com</a> o únase a la comunidad en <a class="a" href="https://www.facebook.com/avira" target="_blank">www.facebook.com/avira</a>.</p>
+			<p>Como la mayor parte de software de antivirus, explora discos duros y extraíbles en busca devirus y también corre como un proceso de fondo, comprobando cada archivo abierto y cerrado. Esto puede descubrir y posiblemente quitar rootkits.</p>
+			<p>Tiene una versión gratuita que contiene las características anteriormente explicadas, y también tiene una versión Premium que mejora sus condiciones y son bastante notables:</p>
+			<p class="espacio">• Detección de adware, phishing</p>
+			<p class="espacio">• Exploración de Correo electrónico (POP3 and SMTP)<br>
+			<p class="espacio">• WebGuard para bloquear acceso a sitios maliciosos<br>
+			<p class="espacio">• Bootable RescueCD para retiro malware y recuperación de sistema</p>
+			<p style="text-align:center"><iframe width="560" height="315" src="https://www.youtube.com/embed/GRKSYKc2UTc" frameborder="0" allowfullscreen></iframe></p>
+    </article>
+	</div>
+	</section>
+	</main>
+	<footer id="foot">Alejandro López Ortiz &copy 2016</footer>
+	</body>
+</html>
---
+++ prueba.php
@@ -0,0 +1,19 @@
+﻿<?php
+$archivo = "contador.txt"; 
+$fp = fopen($archivo, "r+"); 
+if (filesize($archivo)!=0){
+	$old=fread($fp, filesize($archivo));
+	
+}else{
+	$old="";
+}
+rewind($fp);
+$old = $old +1;
+fputs($fp, $old);
+fclose($old);
+
+for($i = 0; $i<strlen($old); $i++){
+	$digito=substr($old, $i, 1);
+	echo $digito;
+}
+?>
